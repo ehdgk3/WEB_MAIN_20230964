d244eb35cebabaab8ed642ffd3946a3796efdf8a
ehdgk3 <rkdqudrn35@naver.com>
2024-06-05 14:12:23 +0900

    14주차

--- js/login.js
+++ js/login.js
@@ -1,12 +1,13 @@
 const check_input = () => {
+    const idsave_check = document.getElementById('idSaveCheck');
     const loginForm = document.getElementById('login_form');
-    const loginBtn = document.getElementById('login_btn');
     const emailInput = document.getElementById('typeEmailX');
     const passwordInput = document.getElementById('typePasswordX');
     const c = '아이디, 패스워드를 체크합니다';
     alert(c);
     const emailValue = emailInput.value.trim();
     const passwordValue = passwordInput.value.trim();
+
     if (emailValue === '') {
         alert('이메일을 입력하세요.');
         return false;
@@ -15,11 +16,15 @@ const check_input = () => {
         alert('비밀번호를 입력하세요.');
         return false;
     }
-    if (emailValue.length < 5) {
+    if (emailValue.length <= 5) {
         alert('아이디는 최소 5글자 이상 입력해야 합니다.');
         return false;
     }
-    if (passwordValue.length < 12) {
+    if(emailValue.length >= 15){                         /* 8주차 응용 문제 */
+        alert('아이디는 최대 15글자까지 입력해야 합니다.');
+        return false;
+    }
+    if (passwordValue.length <= 12) {
         alert('비밀번호는 반드시 12글자 이상 입력해야 합니다.');
         return false;
     }
@@ -36,8 +41,17 @@ const check_input = () => {
     }
     console.log('이메일:', emailValue);
     console.log('비밀번호:', passwordValue);
+
+    if(idsave_check.checked == true) { // 아이디 체크 o
+        alert("쿠키를 저장합니다.", emailValue);
+        setCookie("id", emailValue, 1); // 1일 저장
+        alert("쿠키 값 :" + emailValue);
+    } else { // 아이디 체크 x
+        setCookie("id", emailValue, 0); //날짜를 0 - 쿠키 삭제
+    }
+    session_set();
     loginForm.submit();
-    
+
     const check_xss = (input) => {
         // DOMPurify 라이브러리 로드 (CDN 사용)
         const DOMPurify = window.DOMPurify; // 입력 값을 DOMPurify로 sanitize
@@ -48,20 +62,64 @@ const check_input = () => {
         }
         // Sanitized된 값 반환
         return sanitizedInput;
-        };
+    };
     
-    const sanitizedPassword =   
-    check_xss(passwordValue); // check_xss 함수로 비밀번호 Sanitize
+    const sanitizedPassword = check_xss(passwordValue); // check_xss 함수로 비밀번호 Sanitize
     const sanitizedEmail = check_xss(emailValue); // check_xss 함수로 비밀번호 Sanitize
     if (!sanitizedEmail) {
-    // Sanitize된 비밀번호 사용
         return false;
     }
     if (!sanitizedPassword) {
-    // Sanitize된 비밀번호 사용
         return false;
     }
 };
 
+function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
+    const emailInput = document.getElementById('typeEmailX');
+    const idsave_check = document.getElementById('idSaveCheck');
+    let get_id = getCookie("id");
+    if(get_id) {
+        emailInput.value = get_id;
+        idsave_check.checked = true;
+    }
+    session_check();
+}
+
+function logout(){
+    session_del();
+    location.href='../index.html';
+}
+
+function addJavascript(jsname) { // 자바스크립트 외부 연동
+    var th = document.getElementsByTagName('head')[0];
+    var s = document.createElement('script');
+    s.setAttribute('type','text/javascript');
+    s.setAttribute('src',jsname);
+    th.appendChild(s);
+}
+
+//10주차 응용 문제
+function login_count(){ 
+    var login_Count = getCookie("login_Cnt");
+    if (login_Count == null) {
+        setCookie("login_Cnt", 1, 7);
+    } else {
+        login_Count = parseInt(login_Count) + 1;
+        setCookie("login_Cnt", login_Count, 7); 
+    }
+}
+function logout_count(){
+    var logout_Count = getCookie("logout_Cnt");
+    if (logout_Count == null) {
+        setCookie("logout_Cnt", 1, 7);
+    } else {
+        logout_Count = parseInt(logout_Count) + 1;
+        setCookie("logout_Cnt", logout_Count, 7); 
+    }
+}
+
+addJavascript('/js/security.js'); // 암복호화 함수
+addJavascript('/js/session.js'); // 세션 함수
+addJavascript('/js/cookie.js'); // 쿠키 함수
 
 document.getElementById("login_btn").addEventListener('click', check_input);
